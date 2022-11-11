8055532e13a73354dbd4f84acb2899897a21905c
Brayam Andrey Oliveros Rivera <50060283+AndreyRi13@users.noreply.github.com>
2022-11-11 00:58:33 -0500

    V 2.0

--- src/main/java/com/sso/arquitectura/controller/UserController.java
+++ src/main/java/com/sso/arquitectura/controller/UserController.java
@@ -1,15 +1,16 @@
 package com.sso.arquitectura.controller;
 
+import com.google.gson.Gson;
+import com.sso.arquitectura.dto.TokenDTO;
 import com.sso.arquitectura.dto.UserDTO;
-import org.json.JSONObject;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Properties;
 
 @Controller
 @RequestMapping("/api")
@@ -19,71 +20,74 @@ public class UserController {
 
     private String token;
 
-    @PostMapping("/login")
-    public void login(@RequestParam(value = "user") String user, @RequestParam(value = "password") String password) {
-        final String URL = "http://localhost:3000/login";
-        Model model = null;
-
-        ResponseEntity<UserDTO> user2 = restTemplate.postForEntity(URL, new UserDTO(user, password, 3), UserDTO.class);
-        HttpStatus statusCode = user2.getStatusCode();
-        if (statusCode.compareTo(HttpStatus.valueOf(200)) != 0) {
-            model.addAttribute("message", restTemplate.postForObject(URL, new UserDTO(user, password, 3), UserDTO.class).toString());
-            error(model);
-        }
-        token = user2.toString();
-        welcome();
-
-    }
-
-    @PostMapping("/signoffall")
-    public static void signoffall(@RequestParam(value = "user") String user, @RequestParam(value = "password") String password) {
-        final String URL = "http://localhost:3000/signoffall";
-        Model model=null;
-        ResponseEntity<UserDTO> user2 = restTemplate.postForEntity(URL, new UserDTO(user, password), UserDTO.class);
-        HttpStatus statusCode = user2.getStatusCode();
-        if (statusCode.compareTo(HttpStatus.valueOf(200)) != 0) {
-            model.addAttribute("message", restTemplate.postForObject(URL, new UserDTO(user, password, 3), UserDTO.class).toString());
-            error(model);
-        }
-        loginView();
-
-    }
-
-    @PostMapping("/signoff")
-    public void signoff() {
-        final String URL = "http://localhost:3000/signoff";
-
-
-        ResponseEntity<String> user2 = restTemplate.postForEntity(URL, token, String.class);
-        Model model = null;
-        if (user2.getStatusCode().equals(500)) {
-            error(model);
-        }
-        loginView();
-    }
-
-
     @PostMapping("/register")
-    public static void register(@RequestParam("user") String user, @RequestParam("password") String password, @RequestParam("name") String name) {
+    public String register(@RequestParam("user") String user, @RequestParam("password") String password, @RequestParam("name") String name, RedirectAttributes redirectAttrs) {
         final String URL = "http://localhost:3000/register";
         UserDTO userDTO = new UserDTO();
         userDTO.setUser(user);
         userDTO.setPassword(password);
         userDTO.setName(name);
+        Model model = null;
+        HttpStatus user2 = restTemplate.postForEntity(URL, userDTO, UserDTO.class).getStatusCode();
 
-        ResponseEntity<UserDTO> user2 = restTemplate.postForEntity(URL, userDTO, UserDTO.class);
-        System.out.println(user2.getBody());
-
-        if (user2.getStatusCode().equals(500)) {
-
-            welcome();
+        if (user2.equals(HttpStatus.valueOf(200))) {
+            redirectAttrs
+                    .addFlashAttribute("mensaje", "Creado correctamente")
+                    .addFlashAttribute("clase", "success");
         }
-        registration();
+        return "redirect:/api/registration";
     }
+    @PostMapping("/login")
+    public String login(@RequestParam(value = "user") String user, @RequestParam(value = "password") String password,RedirectAttributes redirectAttrs) {
+        final String URL = "http://localhost:3000/login";
+
+        ResponseEntity<String> json = restTemplate.postForEntity(URL, new UserDTO(user, password, 3), String.class);
+        final Gson gson = new Gson();
+        final TokenDTO tokenD = gson.fromJson(json.getBody(), TokenDTO.class);
+        this.token = tokenD.getToken();
+       if (json.getStatusCode()==HttpStatus.valueOf(403)||json.equals(403)) {
+            redirectAttrs
+                    .addFlashAttribute("mensaje", "Ya existe una sesión activa, debe cerrar sesión en el otro dispositivo")
+                    .addFlashAttribute("clase", "danger");
+            return "redirect:/api/loginview";
+        }
+
+        return "redirect:/api/welcome";
+
+    }
+
+   @PostMapping("/signoffall")
+    public String signoffall(@RequestParam(value = "user") String user, @RequestParam(value = "password") String password,RedirectAttributes redirectAttrs) {
+        final String URL = "http://localhost:3000/signoffall";
+        Model model=null;
+       HttpStatus user2 = restTemplate.postForEntity(URL, new UserDTO(user, password), UserDTO.class).getStatusCode();
+        if (user2.compareTo(HttpStatus.valueOf(200)) != 0) {
+            redirectAttrs
+                    .addFlashAttribute("mensaje", "No se pudieron cerrar las sesiones")
+                    .addFlashAttribute("clase", "danger");
+        }
+       return "redirect:/api/loginview";
+
+    }
+
+    @GetMapping("/signoff")
+    public String signoff(RedirectAttributes redirectAttrs) {
+        final String URL = "http://localhost:3000/signoff";
+        HttpStatus user2 = restTemplate.postForEntity(URL, token, String.class).getStatusCode();
+
+        if (user2!=HttpStatus.valueOf(200)) {
+            redirectAttrs
+                    .addFlashAttribute("mensaje", "No se pudo cerrar la sesion")
+                    .addFlashAttribute("clase", "danger");
+        }
+
+        return "redirect:/api/loginview";
+    }
+
 
     @GetMapping("/")
     public static String inicio() {
-        return "index";
+        return "loginview";
     }
 
     @GetMapping("/welcome")
@@ -91,19 +95,19 @@ public class UserController {
         return "welcome";
     }
 
-    @GetMapping("/loginview")
-    public static String loginView() {
-        return "loginview";
-    }
-
     @GetMapping("/registration")
     public static String registration() {
         return "registration";
     }
 
+    @GetMapping("/loginview")
+    public static String loginView() {
+        return "loginview";
+    }
+
     @GetMapping("/signoffallview")
     public static String signoffallview(Model model) {
-        return "signoffallview";
+        return "signoffview";
     }
 
     @GetMapping("/error")
